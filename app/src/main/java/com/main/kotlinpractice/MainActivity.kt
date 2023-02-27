6be25bbd0b9c4196a0a022349e2123347b7b3706
lqwsy <2567924198@qq.com>
2023-02-27 23:05:55 +0800

    编译与运行程序，配置kotlin插件

--- app/src/main/java/com/main/kotlinpractice/MainActivity.kt
+++ app/src/main/java/com/main/kotlinpractice/MainActivity.kt
@@ -2,10 +2,21 @@ package com.main.kotlinpractice
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initClick()
+    }
+
+    fun initClick(){
+        var btnName: Button = findViewById(R.id.btn_name)
+        btnName.setOnClickListener{
+            toast("点击name")
+        }
     }
 }
---
+++ app/src/main/java/com/main/kotlinpractice/bean/Animal.kt
@@ -0,0 +1,16 @@
+package com.main.kotlinpractice.bean
+
+/**
+ * 版权：咸鱼信息科技有限公司 版权所有
+ * @author lqw
+ * 创建日期：2023/2/27 22:16
+ * 描述：动物类
+ */
+class Animal(name: String, age: Int) {
+
+    init {
+
+    }
+
+
+}
